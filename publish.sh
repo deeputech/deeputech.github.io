763460e9957a5ad3de8549984a12eb8a76db9477
deepu105 <d4udts@gmail.com>
2019-11-14 14:57:36 +0100

    Updated site on Thu Nov 14 14:57:36 CET 2019

--- publish.sh
+++ publish.sh
@@ -6,27 +6,27 @@ if [ -z "$(git status --porcelain)" ]; then
     echo ">>> Working directory clean"
     TMP_LOC=/tmp/deepu.github.io
 
-    /bin/rm -rf _site
-    /bin/rm -rf $TMP_LOC
+    /bin/rm -rf _site || exit
+    /bin/rm -rf $TMP_LOC || exit
 
     echo ">> Building site"
-    bundle update listen
+    bundle update listen || exit
     bundle exec jekyll build
 
     echo ">> Move site to temp folder"
-    mkdir --parents $TMP_LOC
-    mv _site/* $TMP_LOC
+    mkdir --parents $TMP_LOC || exit
+    mv _site/* $TMP_LOC || exit
 
     echo ">> Checkout and clean master"
-    git checkout master
-    find -mindepth 1 -depth -print0 | grep -vEzZ '(temp(/|$)|vendor(/|$)|\.git(/|$)|/\.gitignore$)' | xargs -0 rm -rvf
+    git checkout master || exit
+    find -mindepth 1 -depth -print0 | grep -vEzZ '(temp(/|$)|vendor(/|$)|\.git(/|$)|/\.gitignore$)' | xargs -0 rm -rvf || exit
 
     echo ">> Move site form temp & publish to GitHub"
-    mv $TMP_LOC/* .
+    mv $TMP_LOC/* . || exit
     now=$(date)
-    git add --all
-    git commit -am "Updated site on $now"
-    git push origin master --force
+    git add --all || exit
+    git commit -am "Updated site on $now" || exit
+    git push origin master --force || exit
 
     echo "$now: Published changes to GitHub"
 
