ff021984024311fa64a239aee51dea74d7f7df24
deepu105 <d4udts@gmail.com>
2020-01-21 18:36:10 +0100

    Updated site on Tue Jan 21 18:36:10 CET 2020

--- publish-to-dev.js
+++ publish-to-dev.js
@@ -12,87 +12,95 @@ const HEADERS = {
 
 const axiosInstance = axios.create({
     baseURL: DEV_TO_API_URI,
-    timeout: 2000,
+    timeout: 10000,
     headers: HEADERS
 });
 
-async function postContentToDevTo(payload, id) {
-    try {
-        if (id) {
-            // update the post if its already present
-            return await axiosInstance.put(`${DEV_TO_ARTICLES}/${id}`, payload);
-        }
-        // create the post if it doesn't exist
-        return await axiosInstance.post(DEV_TO_ARTICLES, payload);
-    } catch (error) {
-        console.error(
-            `Failed: ${error.message}, details: ${
-                error.response && error.response.data
-                    ? error.response.data.error
-                    : ""
-            }`
-        );
-        process.exit(1);
-    }
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
 }
 
 const POSTS_DIR = "_posts/";
-try {
-    const filenames = fs.readdirSync(POSTS_DIR);
 
-    if (filenames.length == 0) {
-        console.error(`No files found in path ${POSTS_DIR}`);
-        process.exit(1);
-    }
+async function processFiles() {
+    try {
+        const filenames = fs.readdirSync(POSTS_DIR);
 
-    filenames.forEach(async filename => {
-        const parsedContent = editor.read(filename);
-        const frontMatter = parsedContent.matter.data;
-        const body_markdown = parsedContent.matter.orig;
-        if (frontMatter && frontMatter.published) {
-            console.log("===================================");
-            console.log(`Publish ${filename} to Dev.to: ${frontMatter.title}`);
-            console.log("===================================");
-            const payload = {
-                article: { body_markdown }
-            };
-            let response;
-            if (frontMatter.devto_url) {
-                response = await postContentToDevTo(
-                    payload,
-                    frontMatter.devto_id
-                );
-            } else {
-                response = await postContentToDevTo(payload);
-                if (response) {
-                    // update devto_url & devto_id in the original post
-                    parsedContent
-                        .data((data, matter) => {
-                            data.devto_id = response.data.id;
-                            data.devto_url = response.data.url;
-                            matter.data = data;
-                        })
-                        .save(POSTS_DIR, null, err => {
-                            if (err) {
-                                console.error(err);
-                                process.exit(1);
-                            } else {
-                                console.log(`File ${filename} updated`);
-                            }
-                        });
+        if (filenames.length == 0) {
+            console.error(`No files found in path ${POSTS_DIR}`);
+            process.exit(1);
+        }
+
+        for (const filename of filenames) {
+            try {
+                const parsedContent = editor.read(POSTS_DIR + filename);
+                const frontMatter = parsedContent.matter.data;
+                const body_markdown = parsedContent.matter.orig;
+                if (frontMatter && frontMatter.published) {
+                    console.log("===================================");
+                    console.log(`Publish ${filename} to Dev.to`);
+                    console.log("===================================");
+                    const payload = {
+                        article: { body_markdown }
+                    };
+                    let response;
+                    if (frontMatter.devto_url) {
+                        console.log(`Update post ${frontMatter.devto_url}`);
+                        response = await axiosInstance.put(
+                            `${DEV_TO_ARTICLES}/${frontMatter.devto_id}`,
+                            payload
+                        );
+                    } else {
+                        console.log(`Create post ${frontMatter.titlel}`);
+                        response = await axiosInstance.post(
+                            DEV_TO_ARTICLES,
+                            payload
+                        );
+                        if (response) {
+                            // update devto_url & devto_id in the original post
+                            parsedContent
+                                .data((data, matter) => {
+                                    data.devto_id = response.data.id;
+                                    data.devto_url = response.data.url;
+                                    matter.data = data;
+                                })
+                                .save(POSTS_DIR, null, err => {
+                                    if (err) {
+                                        console.error(err);
+                                        process.exit(1);
+                                    } else {
+                                        console.log(`File ${filename} updated`);
+                                    }
+                                });
+                        }
+                    }
+                    if (response) {
+                        console.log(
+                            `Success: ${response.status} ${response.statusText}`
+                        );
+                        console.log(
+                            `Id: ${response.data.id}, URL: ${response.data.url}`
+                        );
+                    }
+                    await sleep(2000);
                 }
-            }
-            if (response) {
-                console.log(
-                    `Success: ${response.status} ${response.statusText}`
+            } catch (err) {
+                console.error(
+                    `Failed: ${err.message}, details: ${
+                        err.response && err.response.data
+                            ? err.response.data.error
+                            : ""
+                    }`
                 );
-                console.log(
-                    `Id: ${response.data.id}, URL: ${response.data.url}`
-                );
+                process.exit(1);
             }
         }
-    });
-} catch (err) {
-    console.error(err);
-    process.exit(1);
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 }
+
+processFiles();
