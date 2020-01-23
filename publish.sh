99b5ae7a2115bb0eb6bb397a285d5f8d0c6f156d
deepu105 <d4udts@gmail.com>
2020-01-23 22:34:07 +0100

    Updated site on Thu Jan 23 22:34:06 CET 2020

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ if [ -z "$(git status --porcelain)" ]; then
     echo ">> Publish to Dev.to and update slugs"
     npm run publish-to-dev || exit
     git add --all || exit
-    git commit -am "Updated posts with Dev.to slug" || exit
+    git commit --allow-empty -am "Updated posts with Dev.to slug" || exit
 
     echo ">> Building site"
     bundle update listen || exit
