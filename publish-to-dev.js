48aeb4fe263e9c82e4d6f9db8a4d3bcd8e22c416
deepu105 <d4udts@gmail.com>
2020-07-14 14:37:32 +0200

    Updated site on Tuesday 14 July 2020 02:37:31 PM CEST

--- publish-to-dev.js
+++ publish-to-dev.js
@@ -44,7 +44,7 @@ async function processFiles() {
                 const frontMatter = parsedContent.matter.data;
                 const body_markdown = `${json2yaml.stringify(
                     frontMatter
-                )}\n---\n*Originally published in [deepu.tech](https://deepu.tech/${getBlogUrl(filename)}/)*.\n\n${parsedContent.matter.content}`;
+                )}\n---\n*Originally published at [deepu.tech](https://deepu.tech/${getBlogUrl(filename)}/)*.\n\n${parsedContent.matter.content}`;
 
                 if (frontMatter && (frontMatter.published || frontMatter.draft) && !frontMatter.skip_devto) {
                     const payload = {
