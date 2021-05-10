fc739b4e4c9f0682c7051e6bede6aa208de955b2
deepu105 <d4udts@gmail.com>
2021-05-10 10:22:59 +0200

    tweak reading time

--- _plugins/reading_time_filter.rb
+++ _plugins/reading_time_filter.rb
@@ -1,16 +1,16 @@
 # Usage: Read this in {{ page.content | reading_time }}
 
-WORD_PER_MINUTE = 180
+WORD_PER_MINUTE = 200
 
-def calculate_time( input )
-  words = input.split.size;
-  minutes = ( words / WORD_PER_MINUTE ).floor
+def calculate_time(input)
+  words = input.split.size
+  minutes = (words / WORD_PER_MINUTE).floor
   minutes_label = minutes == 1 ? "min" : "mins"
   return minutes, minutes_label
 end
 
 module ReadingTimeFilter
-  def reading_time( input )
+  def reading_time(input)
     minutes, minutes_label = calculate_time(input)
     minutes > 0 ? "#{minutes} #{minutes_label} read" : "Less than 1 min read"
   end
