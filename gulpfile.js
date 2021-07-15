309fc82476fd29647a94890887147f7ec87e285c
deepu105 <d4udts@gmail.com>
2021-07-15 12:18:50 +0200

    Updated site on Thursday 15 July 2021 12:18:49 PM CEST

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,45 @@ const imagemin = require("gulp-imagemin");
 
 // Gulp task to minify CSS files
 gulp.task("styles", () =>
-    gulp
-        .src("./_site/assets/css/*.css")
-        // Auto-prefix css styles for cross browser compatibility
-        .pipe(autoprefixer())
-        // Minify the file
-        .pipe(csso())
-        // Output
-        .pipe(gulp.dest("./_site/assets/css"))
+  gulp
+    .src("./_site/assets/css/*.css")
+    // Auto-prefix css styles for cross browser compatibility
+    .pipe(autoprefixer())
+    // Minify the file
+    .pipe(
+      csso({
+        sourceMap: false,
+      })
+    )
+    // Output
+    .pipe(gulp.dest("./_site/assets/css"))
 );
 
 // Gulp task to minify JavaScript files
 gulp.task("scripts", () =>
-    gulp
-        .src("./_site/assets/js/*.js")
-        // Minify the file
-        .pipe(uglify())
-        // Output
-        .pipe(gulp.dest("./_site/assets/js"))
+  gulp
+    .src("./_site/assets/js/*.js")
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(gulp.dest("./_site/assets/js"))
 );
 
 // Gulp task to minify HTML files
 gulp.task("pages", () =>
-    gulp
-        .src(["./_site/**/*.html"])
-        .pipe(
-            htmlmin({
-                collapseWhitespace: true,
-                removeComments: true
-            })
-        )
-        .pipe(gulp.dest("./_site"))
+  gulp
+    .src(["./_site/**/*.html"])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true,
+      })
+    )
+    .pipe(gulp.dest("./_site"))
 );
 
-gulp.task("images", () =>
-    gulp
-        .src(["./assets/images/**/*"])
-        .pipe(imagemin())
-        .pipe(gulp.dest("./assets/images"))
-);
+gulp.task("images", () => gulp.src(["./assets/images/**/*"]).pipe(imagemin()).pipe(gulp.dest("./assets/images")));
 
 gulp.task("default", gulp.parallel("scripts", "styles", "pages"));
