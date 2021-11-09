5fc8035b444b9f546a31c0844ee4f7571aaf0a69
Deepu <d4udts@gmail.com>
2021-11-09 12:19:20 +0100

    Updated site on Tue Nov  9 12:19:19 PM CET 2021

--- assets/js/ie10-viewport-bug-workaround.js
+++ assets/js/ie10-viewport-bug-workaround.js
@@ -1,1 +1,1 @@
-!function(){"use strict";if(navigator.userAgent.match(/IEMobile\/10\.0/)){var e=document.createElement("style");e.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.head.appendChild(e)}}();
+!function(){"use strict";var e;navigator.userAgent.match(/IEMobile\/10\.0/)&&((e=document.createElement("style")).appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.head.appendChild(e))}();
